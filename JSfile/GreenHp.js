b6ef49e48d50dc11ed16dcc2706cc7c27a2db9fb
Simdajung <ekwjd1354@naver.com>
2023-04-05 14:01:55 +0900

    project init

---
+++ JSfile/GreenHp.js
@@ -0,0 +1,43 @@
+let myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mainSlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+ 
+//메뉴 화면 
+const fixbox =document.getElementById("fixbox");
+const page1 = document.getElementById('introlist');
+const page2 = document.getElementById('activelist');
+const page3 = document.getElementById('bookinglist');
+const page4 = document.getElementById('surfShoplist');
+
+page1.onclick = function() {
+    fixbox.src="../Reproject/intro.html";
+}
+page2.onclick = function() {
+    fixbox.src="../Reproject/active.html";
+}
+page3.onclick = function() {
+    fixbox.src="../Reproject/lesson.html";
+}
+page4.onclick = function() {
+    fixbox.src="../Reproject/surfShop.html";
+}
+
+//사업자정보 아코디언
+$(".que").click(function () {
+    $(this).next(".anw").stop().slideToggle(300);
+    $(this).toggleClass('on').siblings().removeClass('on');
+    $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+});
+
+//풋터
+document.write(new Date().getFullYear());
---
+++ JSfile/lesson.js
@@ -0,0 +1,7 @@
+
+function level1(){
+    document.getElementById('level1').addEventListener('click',function(){
+        document.getElementById('레벨1설명').style.display='block';
+
+    }) 
+} 
